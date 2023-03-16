2eadd65893587aa66c05cf5ac8cda978edce91ca
pedro Vinicius <singed161@gmail.com>
2023-03-15 21:56:18 -0300

    css

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,9 +1,21 @@
-
-
+import {FiSearch} from 'react-icons/fi'
+import './styles.css'
 function App() {
   return (
-    <div className="App">
-      <h1>Teste Projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscador Cep</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu Cep..." />
+        <button className = "buttonSearch"> <FiSearch size={25} color="#FFF"/> </button>
+      </div>
+      <main className='main'>
+        <h2> Cep: </h2>
+        <span> Rua Teste </span>
+        <span> Complemento: Teste </span>
+        <span> Bairro: Teste </span>
+        <span> Cidade Teste - TS </span>
+
+      </main>
     </div>
   );
 }
