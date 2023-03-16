0dde4c935af2885b0593cbd0063e06d54846ff72
pedro Vinicius <singed161@gmail.com>
2023-03-15 23:01:16 -0300

    add

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,12 +1,23 @@
+import {useState} from 'react';
 import {FiSearch} from 'react-icons/fi'
 import './styles.css'
 function App() {
+  const [input,setinput] = useState('')
+
+  function handleSearch(){
+    alert("Test")
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador Cep</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu Cep..." />
-        <button className = "buttonSearch"> <FiSearch size={25} color="#FFF"/> </button>
+        <input type="text" placeholder="Digite seu Cep..."
+        value={input}
+        onChange ={(e) => setinput(e.target.value)}
+        />
+        <button className = "buttonSearch" onClick={handleSearch} >
+           <FiSearch size={25} color="#FFF"/> </button>
       </div>
       <main className='main'>
         <h2> Cep: </h2>
